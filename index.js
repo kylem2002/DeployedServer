a20623267839e51aad5802ff850072fd8da0bc5d
kylem2002 <146904543+kylem2002@users.noreply.github.com>
2023-12-06 06:35:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -85,5 +85,5 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5501;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
