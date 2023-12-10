0c688f2722be1c028fe647c00908ff222520417f
kylem2002 <146904543+kylem2002@users.noreply.github.com>
2023-12-10 18:51:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,60 +3,72 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
-
-    // Extension of the file
-    let extname = path.extname(filePath);
-
-    // Initial content type
-    let contentType = 'text/html';
-
-    // Check ext and set content type
-    switch (extname) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.jpeg':
-            contentType = 'image/jpeg';
-            break;
-        // other cases for different file types can be added here
-    }
-
-    // Check if contentType is text/html but no .html file extension
-    if (contentType == "text/html" && extname == "") filePath += ".html";
-
-    // Log the filePath
-    console.log(filePath);
-
-    // Read File
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if (err.code == 'ENOENT') {
-                // Page not found
-                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.end(content, 'utf8');
-                });
-            } else {
-                // Some server error
-                res.writeHead(500);
-                res.end(`Server Error: ${err.code}`);
+    // Handle API request
+    if (req.url === '/api') {
+        fs.readFile(path.join(__dirname, 'public', 'db.json'), 'utf-8',
+            (err, content) => {
+                if (err) throw err;
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(content);
             }
-        } else {
-            // Success
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf8');
+        );
+    } else {
+        // Build file path
+        let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+        // Extension of the file
+        let extname = path.extname(filePath);
+
+        // Initial content type
+        let contentType = 'text/html';
+
+        // Check ext and set content type
+        switch (extname) {
+            case '.css':
+                contentType = 'text/css';
+                break;
+            case '.json':
+                contentType = 'application/json';
+                break;
+            case '.png':
+                contentType = 'image/png';
+                break;
+            case '.jpg':
+                contentType = 'image/jpg';
+                break;
+            case '.jpeg':
+                contentType = 'image/jpeg';
+                break;
+            // other cases for different file types can be added here
         }
-    });
+
+        // Check if contentType is text/html but no .html file extension
+        if (contentType == "text/html" && extname == "") filePath += ".html";
+
+        // Log the filePath
+        console.log(filePath);
+
+        // Read File
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                if (err.code == 'ENOENT') {
+                    // Page not found
+                    fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.end(content, 'utf8');
+                    });
+                } else {
+                    // Some server error
+                    res.writeHead(500);
+                    res.end(`Server Error: ${err.code}`);
+                }
+            } else {
+                // Success
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.end(content, 'utf8');
+            }
+        });
+    }
 });
 
 const PORT = process.env.PORT || 5959;
