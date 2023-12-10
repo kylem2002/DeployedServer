a82ababf502a515c3bc94507db83ca94b6c2343d
kylem2002 <146904543+kylem2002@users.noreply.github.com>
2023-12-10 18:03:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,79 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const cors = require('cors'); 
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const app = express();
+const server = http.createServer((req, res) => {
+    
+    /*
 
-// Use CORS middleware (Allow all origins for simplicity. Adjust as needed.)
-app.use(cors());
+        
 
-app.use(express.static(path.join(__dirname, 'public')));
+        we can Navigate to different pages via different requests. 
+        if / then goto index.html
+        if /about about then goto about.html
+        if /api then laod the JSON file  /  ;) this might be something you need for your exam. 
 
-// API endpoint to serve data from db.json
-app.get('/api', (req, res) => {
-    fs.readFile(path.join(__dirname, 'public', 'db.json'), 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('Server Error: Unable to read db.json');
-        } else {
-            // Parse data as JSON and send it
-            res.json(JSON.parse(data));
-        }
-    });
+
+
+    */
+   
+    
+    
+    if (req.url === '/') {
+        // read public.html file from public folder
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                                    res.end(content);
+                        }
+              );
+     }
+
+    else if (req.url === '/about') {
+
+
+        // read the about.html file public folder
+        fs.readFile(
+            path.join(__dirname, 'public', 'about.html'),
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                                    res.end(content);
+                        }
+              );
+     }
+    else if (req.url==='/api')
+    {
+        fs.readFile(
+            path.join(__dirname, 'public', 'db.json'),'utf-8',
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    // Please note the content-type here is application/json
+                                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                                    res.end(content);
+                        }
+              );
+    }
+    else{
+        res.end("<h1> 404 nothing is here</h1>");
+    }
+
+    /*
+
+        But what if we have  1000 pages/urls ? do we need to write 1000 if-else statements?
+
+    /*/
 });
 
-// GET requests not handled before 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// it will first try to look for
+// environment variable, if not found then go for 5959
+const PORT= process.env.PORT || 5959;
 
+// port, callback
+server.listen(PORT,()=> console.log(`Great our server is running on port ${PORT} `));
 const PORT = process.env.PORT || 5501;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
